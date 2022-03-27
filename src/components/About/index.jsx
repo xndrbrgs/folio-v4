3359b238a3143a52efbf0506fc682bc02c5fc8ef
xndrbrgs <aborgescolon@gmail.com>
2022-03-26 21:44:10 -0400

    editing rem in first 3 components

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -25,14 +25,17 @@ export default function About() {
   }, [])
   return (
     <div class="about-me" id="about-me" data-scroll-section>
-      <div>
         <h1 data-scroll>About Me</h1>
+      <div className="wrapper">
+        <section data-scroll className="about-section">
         <p data-scroll className="bolden-p">
-          I am a full-stack and creative developer based in Orlando, Florida. I am a Biomedical Sciences graduate who later switched to the amazing world of web development.
+          I am a full-stack creative developer based in Orlando, Florida. I am a Biomedical Sciences graduate who later switched to the amazing world of web development.
         </p>
         <p data-scroll className="smaller-p">
-          I specialize in creating integrative and intuitive code that enables a placid user experience.   
+          I specialize in creating integrative and intuitive code that enables a placid user experience.    
+          My development stack is focused on performace and accessibility, making any user feel delighted with their site interactions.     
         </p>
+        </section>
       </div>
     </div>
   );
